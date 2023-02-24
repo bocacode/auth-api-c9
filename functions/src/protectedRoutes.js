a50a53af039c28939cc28f8cce58cee388faf025
Todd Albert <todd@bocacode.com>
2023-02-24 15:21:13 -0500

    all zipped up!

--- functions/src/protectedRoutes.js
+++ functions/src/protectedRoutes.js
@@ -1,5 +1,11 @@
-
+import jwt from 'jsonwebtoken'
+import { secret_key } from '../service_account.js'
 
 export function getSecrets(req, res) {
+  const decoded = jwt.verify(req.headers.authorization, secret_key)
+  // if(!decoded) {
+  //   res.status(403).send({ message: 'Not authorized!' })
+  //   return
+  // }
   res.send({ message: 'Buy Bitcoin Weekly!' })
 }
